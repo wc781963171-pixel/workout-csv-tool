db455618e58b5814508ee6e12680e38b7530a444
wc781963171 <wc781963171@gmail.com>
2026-04-17 10:37:33 +0800

    add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,11 @@
+	import { MetadataRoute } from 'next'
+	export default function sitemap(): MetadataRoute.Sitemap {
+	  return [
+	    {
+	      url: 'https://aiworkingout.top',
+	      lastModified: new Date(),
+	      changeFrequency: 'monthly',
+	      priority: 1,
+	    },
+	  ]
+	}
